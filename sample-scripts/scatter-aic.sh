0fe4e6cad45f16132a44ac1a582ef4d9d1f42e3e
Vladimir Menkov <vmenkov@gmail.com>
2021-09-19 14:21:20 -0400

    more gnu plots

--- sample-scripts/scatter-aic.sh
+++ sample-scripts/scatter-aic.sh
@@ -8,12 +8,13 @@ echo Rules=$rules
 set p=`echo $rules | sed -e 's|/|-|g'`
 grep $rules summary-flat.csv | cut -d , -f 14 > aic-flat-$p.csv
 grep $rules summary-p0-COMPLETELY_RANDOM.csv | cut -d , -f 14 > aic-random-$p.csv
-paste  aic-flat-$p.csv  aic-random-$p.csv > aic-scatter-$p.dat
+grep $rules summary-p0-MCP1.csv | cut -d , -f 14 > aic-mcp1-$p.csv
+paste  aic-flat-$p.csv  aic-random-$p.csv aic-mcp1-$p.csv > aic-scatter-$p.dat
 
 
 echo "set term x11" >> tmp.gnu
 echo "set title 'Rules: $rules'" >> tmp.gnu
-echo "plot 'aic-scatter-$p.dat' with points;" >> tmp.gnu
+echo "plot 'aic-scatter-$p.dat'" 'using ($1):($2) with points' >> tmp.gnu
 echo "set out 'aic-scatter-$p.png'" >> tmp.gnu
 echo "set term png" >> tmp.gnu
 echo "replot" >> tmp.gnu
