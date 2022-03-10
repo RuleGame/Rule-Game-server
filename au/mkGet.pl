0014276e60af8ff79672fea2ef49240147cdc658
Vladimir Menkov <vmenkov@gmail.com>
2022-03-10 14:05:53 -0500

    docs

--- au/mkGet.pl
+++ au/mkGet.pl
@@ -5,6 +5,7 @@ use strict;
 #------------------------------------------
 # Input:
 #   private String id; 
+#   String id1, id2; 
 # Output:
 #   Get and Set methods
 #-------------------------------------------
@@ -19,6 +20,16 @@ while(defined($s=<>)) {
     ($s =~ /;$/) or next;
     $s =~ s/\s*;$//;
     $s =~ s/\s*=.*//;
+
+    my @names = ();
+    while($s =~ /\s*,\s*(\S+)$/) {
+	# print "Remove last var $1\n";
+	@names = (@names,$1);
+	$s = $`;
+    }
+
+
+
     my @q = split( /\s+/, $s);
     while (scalar(@q)>1) {
 	if ($q[0] eq "private")	{ shift @q;  }
@@ -28,10 +39,14 @@ while(defined($s=<>)) {
 
     scalar(@q)==2 or die  "Wrong number of tokens in: $s\n";
     my ($type,$name) = @q;
-    my $Name = &cap1($name);
-    print "${indent}public ${type} get${Name}() \{ return $name; }\n";
-    print $indent . '@' . "XmlElement\n";
-    print "${indent}public void set${Name}(${type} _$name) \{ $name = _$name; }\n";
+    @names = ($name, @names);
+
+    foreach $name (@names) {
+	my $Name = &cap1($name);
+	print "${indent}public ${type} get${Name}() \{ return $name; }\n";
+	print $indent . '@' . "XmlElement\n";
+	print "${indent}public void set${Name}(${type} _$name) \{ $name = _$name; }\n";
+    }
     
 }
     
