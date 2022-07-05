7a1635ec73ff459baa2b8312354f79de8ab99210
Vladimir Menkov <vmenkov@gmail.com>
2022-07-05 15:36:13 -0400

    one more script made safe for Python 3

--- python/client-socket.py
+++ python/client-socket.py
@@ -23,26 +23,26 @@ port=int(sys.argv[2])
 
 otherParamString = " ".join(sys.argv[3:])
 
-# game=sys.argv[3]
+game=sys.argv[3]
 
 #-- this is a string, not a number!
-# nPieces = sys.argv[4]
+nPieces = sys.argv[4]
 
-#sys.stdout.write("Port="+repr(port)+" Rule file=" + game +", #pieces=" + nPieces+"\n")
+sys.stdout.write("Port="+repr(port)+" Rule file=" + game +", #pieces=" + nPieces+"\n")
 
 sys.stdout.write("Host=" + host+", Port="+repr(port)+", game params: " + otherParamString +"\n")
 
 try: 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    print "Socket successfully created"
+    print( "Socket successfully created")
 except socket.error as err: 
-    print "socket creation failed with error %s" %(err) 
+    print( "socket creation failed with error {0}".format(err) )
 
 try: 
     host_ip = socket.gethostbyname(host) 
 except socket.gaierror: 
     # this means could not resolve the host 
-    print "there was an error resolving the host"
+    print( "there was an error resolving the host")
     sys.exit() 
   
     
@@ -50,10 +50,10 @@ except socket.gaierror:
 try: 
     sock.connect((host_ip, port))
 except socket.error as err:
-    print "socket connect failed with error %s" %err 
+    print( "socket connect failed with error {0}".format(err) )
     sys.exit() 
 
-print "the socket has successfully connected to host=" + host_ip+ " on port=" + repr(port)
+print( "the socket has successfully connected to host=" + host_ip+ " on port=" + repr(port))
 
 
 #-- see https://docs.python.org/2/library/socket.html
@@ -64,8 +64,8 @@ wfile = sock.makefile('wb')
 # msg = "GAME \"" + game + "\" " + nPieces
 msg = "GAME " + otherParamString 
 
-print "Sending: " + msg + "\n"
-wfile.write(msg + "\n")
+print( "Sending: " + msg + "\n")
+wfile.write((msg + "\n").encode())
 wfile.flush()
 
 gameLoop.mainLoop(rfile, wfile)
