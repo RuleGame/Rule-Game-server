6d77125c1c03a352ed1237d4ac3fafb52d26e817
Vladimir Menkov <vmenkov@gmail.com>
2025-11-07 00:32:07 -0500

    one more file... for deterministic "jitter"

---
+++ src/edu/wisc/game/svg/NoOverlap.java
@@ -0,0 +1,72 @@
+package edu.wisc.game.svg;
+
+import java.io.*;
+import java.util.*;
+
+import  edu.wisc.game.util.Util;
+
+/** An auxiliary class for Curve; used to help "jitter" some curves overlapping
+    sections, so that they won't overlap.
+*/
+class NoOverlap extends HashMap<Double, NoOverlap.Horizontal> {
+    static class Segment {
+	final int x0, x1;
+	int offset=0;
+	Segment(int _x0, int _x1) {
+	    x0 = _x0;
+	    x1 = _x1;
+	}
+	/** @return true if the two segment overlap over some length
+	    (not just at a single end point) */
+	boolean overlaps(Segment q) {
+	    return (q.x0 <= x0) ?  q.x1 > x0:
+		(q.x0 < x1);
+	}
+    }
+
+    /** Stores information about all horizontal curve segments that 
+	are located at a particular y */
+    static class Horizontal extends Vector<Segment> {
+	/** math coordinates */
+	double y;
+	Horizontal(double _y) {	    
+	    y = _y;
+	}
+	int doAdd(Segment seg) {
+	    int maxOffset = -1;
+	    for(Segment q: this) {
+		if (q.overlaps(seg)) {
+		    maxOffset = Math.max(maxOffset, q.offset);
+		}
+	    }
+	    seg.offset = maxOffset+1;
+	    add(seg);
+	    return seg.offset;	       
+	}
+    }
+
+    /** @return an array with the recommended integer offset for each
+	point of the curve. */
+    int[] add(Curve c) {
+	int[] offset = new int[c.y.length];
+	for(int j=0; j<c.y.length-1 ||j == c.y.length-1 && c.extraX!=null; j++) {
+	    int x0 = j, x1 = (j+1 < c.y.length)? j+1: (int)c.extraX.doubleValue();
+	    double y0 =  c.y[j];
+	    double y1 = (j+1 < c.y.length)? c.y[j+1]: y0;
+
+	    if (y1 != y0) continue;
+
+	    Double key = new Double(y0);
+	    Horizontal h = get(key);
+	    if (h==null) put(key, h=new Horizontal(y0));
+	    int d = h.doAdd(new Segment(x0,x1));
+	    if (d!=0) {
+		offset[j] = d;
+		if (j+1<c.y.length) offset[j+1] = d;
+	    }
+	}
+
+	return offset;
+    }
+
+}
